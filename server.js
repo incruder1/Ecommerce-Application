69f42d271c26cd9b6167304efcd47e9289721bcc
Dev Johri <56020041+incruder1@users.noreply.github.com>
2023-11-06 10:17:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
-
+import path from 'path'
 //configure env
 dotenv.config();
 
@@ -21,7 +21,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-
+app.use(express.static(path.join(__dirname,'./client/build')))
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
@@ -31,6 +31,10 @@ app.use("/api/v1/product", productRoutes);
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to Time Zone</h1>");
 });
+
+app.use("*", function(req,res){
+  res.sendFile(path.join(__dirname,".client/build/index.html"));
+})
 
 //PORT
 const PORT = process.env.PORT || 8080;
